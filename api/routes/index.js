eb04f629310948e6d8aa1bfcbca82354a404d3dc
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-27 16:30:42 -0300

    A route was create to receive a Json and the response is the same object

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.post('/api/v1/webhook_jira', function(req, res, next) {
+  //res.render('webhook', { title: req.body });
+  res.json(req.body)
+});
+
 module.exports = router;
