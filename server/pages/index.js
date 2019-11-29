57e68dadf3ef830b46085a1e612734d5753b2643
erasmo.pinheiro <erasmosouza@gmail.com>
2019-11-29 12:50:14 -0300

    Hello Material-ui core and icon

--- server/pages/index.js
+++ server/pages/index.js
@@ -1,8 +1,14 @@
+import Typography from '@material-ui/core/Typography';
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
+
 export default function Index() {
     return (
-      <div>
-        <p>Hello Next.js</p>
-      </div>
+    <div>
+        <p>
+            <HomeWorkIcon color="primary" />
+            <Typography variant="subtitle1">Hello Next.js</Typography>
+        </p>
+    </div>
     )
   }
   
